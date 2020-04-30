5550ebe420b6b801aa62634ecb42a12c2f4dda51
sk814 <sanjeev.cs.singh@gmail.com>
2020-04-30 01:32:07 +0100

    try to url upload

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,18 @@ from .form import Blog
 from .models import Blog_Model
 
 def all_blogs(request):
+
+
+    if request.method == 'GET':
         blog_obj = Blog_Model.objects.all()
         return render(request, 'blog/all_blogs.html', {'blog_obj': blog_obj})
+    else:
+        try:
+            form=Blog(request.POST)
+            form.save(commit=True)
+        except ValueError:
+            return render(request, 'blog/all_blogs.html',  {'form': Blog(),'error':'Bad data passed in. Please try again'})
+
 
 
 
