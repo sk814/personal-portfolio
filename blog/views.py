4532e06b173e3ce0cdf78a48587dd8dd6345260a
sk814 <sanjeev.cs.singh@gmail.com>
2020-05-01 17:21:53 +0100

    url done

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .form import Blog
 from .models import Blog_Model
 
+
 def all_blogs(request):
-
-
     if request.method == 'GET':
-        blog_obj = Blog_Model.objects.all()
-        return render(request, 'blog/all_blogs.html', {'blog_obj': blog_obj})
+        blog_obj = Blog_Model.objects.all().order_by('-id')
+        return render(request, 'blog/all_blogs.html', {'form': Blog(),'blog_obj':blog_obj})
     else:
         try:
-            form=Blog(request.POST)
-            form.save(commit=True)
+            form = Blog(request.POST)
+            newurl = form.save(commit=False)
+            newurl.save()
+            return redirect(request.path_info)
         except ValueError:
             return render(request, 'blog/all_blogs.html',  {'form': Blog(),'error':'Bad data passed in. Please try again'})
-
-
-
-
 
