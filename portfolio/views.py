db866049048940eb9fca5991ea40de237ee1e18f
sk814 <sanjeev.cs.singh@gmail.com>
2020-06-14 22:45:15 +0100

    Picture galary

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from .models import Projects
 
 def media(request):
-    projects=Projects.objects.all()
+    projects=Projects.objects.all().order_by('-id')
     return render(request,'portfolio/homepage.html',{'projects':projects})
 
